0a1beef89af682038af339e037e70395d4e3f22a
Kirill <111251916+Cirillo-f@users.noreply.github.com>
2025-03-29 00:38:50 +0300

    final

--- backend/internal/handlers/postScan.go
+++ backend/internal/handlers/postScan.go
@@ -14,34 +14,30 @@ type ScanHandler struct{}
 func NewScanHandler(router *http.ServeMux) {
 	handler := ScanHandler{}
 
-	// Регистрируем обработчик для /scan
 	router.HandleFunc("/scan", handler.scanner())
 }
 
 func (handler *ScanHandler) scanner() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Устанавливаем CORS заголовки на каждый запрос
-		w.Header().Set("Access-Control-Allow-Origin", "*")              // Разрешаем доступ с любых доменов
-		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS") // Разрешаем методы POST и OPTIONS
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")  // Разрешаем Content-Type заголовки
+		w.Header().Set("Access-Control-Allow-Origin", "*")              
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS") 
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")  
 
-		// Если это запрос OPTIONS, сразу завершаем его без дальнейшей обработки
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Быстрая проверка тела запроса для POST
 		body, err := utils.HandleBody(&w, r)
 		if err != nil {
 			res.Json(w, err.Error(), http.StatusPaymentRequired)
 			return
 		}
 
-		// Запускаем сканирование
+
 		openPorts := scanner.TCPScanner(body.Host)
 
-		// Отправляем ответ
+
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"host":       body.Host,
